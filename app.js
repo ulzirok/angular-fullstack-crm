5121a76b18178e437230e586748869a302484a56
ulzirok <ulzirok@gmail.com>
2025-12-02 20:56:29 +0500

    connect the cors and morgan packages to handle cors and to log info about requests in the terminal

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express'); //подключаем файл express
 const app = express(); //создаем сервер (экзепляр фреймворка express)
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser') //для парсинга данных
+const cors = require('cors') //для обработки CORS-запросов
+
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 
+app.use(require('morgan')('dev'))
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/category', categoryRoutes);
