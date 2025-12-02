1070cb6cbc6d465715697fd9f47b9292b5b4251d
ulzirok <ulzirok@gmail.com>
2025-12-02 20:43:53 +0500

    connect the Postman package for testing requests and Body-parser for parsing data

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express'); //подключаем файл express
 const app = express(); //создаем сервер (экзепляр фреймворка express)
+const bodyParser = require('body-parser')
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/category', categoryRoutes);
