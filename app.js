2000e1d424f2401e275aaae5f6ef78f6b6daf42e
ulzirok <ulzirok@gmail.com>
2025-12-02 19:56:29 +0500

    create routes and controllers to handle requests from the client

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express'); //подключаем файл express
+const app = express(); //создаем сервер (экзепляр фреймворка express)
+const authRoutes = require('./routes/auth');
+const analyticsRoutes = require('./routes/analytics');
+const categoryRoutes = require('./routes/category');
+const orderRoutes = require('./routes/order');
+const positionRoutes = require('./routes/position');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/analytics', analyticsRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/order', orderRoutes);
+app.use('/api/position', positionRoutes);
+
+module.exports = app;
