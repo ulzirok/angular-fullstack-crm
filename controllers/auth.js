a3df3eed1fd80263eebe073fdc7d5154c858f61f
ulzirok <ulzirok@gmail.com>
2025-12-03 15:46:58 +0500

    implement email verification logic in user registration and hash the password by the bcrypt-package

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/User');
+
 module.exports.login = function (req, res) {
   res.status(200).json({
     login: {
@@ -7,8 +10,28 @@ module.exports.login = function (req, res) {
   });
 };
 
-module.exports.register = function (req, res) {
-  res.status(200).json({
-    register: 'from controller'
-  });
-}
+module.exports.register = async function (req, res) {
+  const candidate = await User.findOne({ email: req.body.email }); //в БД проверяем есть ли такой email (ч/з mongoose)
+  if (candidate) { //если есть
+    res.status(409).json({ //клиенту вернем ошибку
+      message: 'Такой email уже занят. Попробуйте другой'
+    });
+  }
+  else { //если нет, создаем пользователя (но пароль хэшируем)
+    const salt = bcrypt.genSaltSync(10); //генерируем хэш для пароля клиента
+    const password = req.body.password //сохраняем пароль клиента в переменную
+    
+    const user = new User({ //локально сохраняем
+      email: req.body.email,
+      password: bcrypt.hashSync(password, salt) //хэш пароля+salt
+    });
+
+    try { //если БД отвечает
+      await user.save(); //в БД сохраняем
+      res.status(201).json(user); //клиенту вернем объект
+    }
+    catch (err) { //если БД не отвечает
+      //Обработать ошибку 
+    }
+  }
+};
