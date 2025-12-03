713e910e6cf874451d026c5f4fa6d6862097eb24
ulzirok <ulzirok@gmail.com>
2025-12-03 22:21:48 +0500

    re-add the initial Angular frontend setup (without standalone)

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
+  standalone: false,
   styleUrl: './app.scss'
 })
 export class App {
