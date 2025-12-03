51bd93d79c98e0f1eb41887c442ec791993aebce
ulzirok <ulzirok@gmail.com>
2025-12-03 16:49:38 +0500

    implement login logic and jwt-token generation using the jsonwebtoken-package

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,12 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User');
-
-module.exports.login = function (req, res) {
-  res.status(200).json({
-    login: {
-      email: req.body.email,
-      password: req.body.password
-    }
-  });
-};
+const keys = require('../config/keys')
 
 module.exports.register = async function (req, res) {
-  const candidate = await User.findOne({ email: req.body.email }); //в БД проверяем есть ли такой email (ч/з mongoose)
+  const candidate = await User.findOne({ email: req.body.email }); //в БД ищем есть ли пользователь с таким email (ч/з mongoose)
   if (candidate) { //если есть
-    res.status(409).json({ //клиенту вернем ошибку
+    res.status(409).json({ //клиенту вернем ошибку 409
       message: 'Такой email уже занят. Попробуйте другой'
     });
   }
@@ -28,10 +21,38 @@ module.exports.register = async function (req, res) {
 
     try { //если БД отвечает
       await user.save(); //в БД сохраняем
-      res.status(201).json(user); //клиенту вернем объект
+      res.status(201).json(user); //клиенту вернем объект 201
     }
     catch (err) { //если БД не отвечает
       //Обработать ошибку 
     }
+  }
+};
+
+module.exports.login = async function (req, res) {
+  const candidate = await User.findOne({ email: req.body.email }); //в БД ищем есть ли пользователь с таким email (ч/з mongoose)
+  if (candidate) { //если есть
+    const passwordResult = bcrypt.compareSync(req.body.password, candidate.password); //сравниваем пароль клиента с паролем в БД
+    if (passwordResult) { //если пароли совпадают, генерируем jwt-токен
+      
+      const token = jwt.sign({ //методу передаем 3 аргумента для получения jwt-токена
+        email: candidate.email,
+        userId: candidate._id
+      }, keys.jwt, {expiresIn: 60 * 60})
+      
+      res.status(200).json({ //клиенту вернем токен, чтобы клиент мог далее отсылать запросы
+        token: `Bearer ${token}`
+      })
+    }
+    else { //если не совпадают
+      res.status(401).json({ //клиенту вернем ошибку 401
+        message: 'Пароли не совпадают. Попробуйте снова'
+      }) 
+    }
+  }
+  else { //если нет
+    res.status(404).json({ //клиенту вернем ошибку 404
+      message: 'Пользователь с таким email не найден'
+    })
   }
 };
