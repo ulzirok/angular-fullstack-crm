d835c86433f1de9c366702962c597b83d71e524e
ulzirok <ulzirok@gmail.com>
2025-12-03 18:00:08 +0500

    add the passportjs-package to protect routes from unauthorized users

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require('express'); //подключаем файл express
 const app = express(); //создаем сервер (экзепляр фреймворка express)
-const bodyParser = require('body-parser') //для парсинга данных
+const bodyParser = require('body-parser'); //для парсинга данных
 const cors = require('cors'); //для обработки CORS-запросов
-const mongoose = require('mongoose')
-const keys = require('./config/keys')
+const mongoose = require('mongoose');
+const passport = require('passport');
 
+const keys = require('./config/keys');
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 
-mongoose.connect(keys.mongoURI)
-.then(() => console.log('Mongodb connected'))
-.catch((err) => console.log(err))
+mongoose.connect(keys.mongoURI) //инициализация подключения с БД (mongoDB)
+  .then(() => console.log('Mongodb connected'))
+  .catch((err) => console.log(err));
 
-app.use(require('morgan')('dev'))
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(passport.initialize());
+require('./middleware/passport')(passport);
+
+app.use(require('morgan')('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
---
+++ middleware/passport.js
@@ -0,0 +1,30 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const keys = require('../config/keys');
+
+//Защита наших роутов от неавторизованных пользователей (если в запросах в Headers есть токен - разрешаем переходить по роуту, если нет - запрещаем Unauthorized)
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: keys.jwt
+};
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(options, async (payload, done) => {
+      const user = await User.findById(payload.userId).select('email id'); //в БД ищем пользователя
+      try {
+        if (user) { //если есть, добавляем данные к запросам
+          done(null, user);
+        }
+        else { //если нет, ничего не добавляем
+          done(null, false);
+        }
+      }
+      catch (err) {
+        console.log(err);
+      }
+    })
+  );
+};
